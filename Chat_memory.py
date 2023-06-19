f33374d910c280a9182e4b103c243809083b5750
kotaro <big.i.land.k@gmail.com>
2023-06-19 13:23:43 +0900

    modified propmt

--- Chat_memory.py
+++ Chat_memory.py
@@ -1,3 +1,17 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+
+
+chat = ChatOpenAI(temperature=0)
+
 target_text= "根室本線は北海道の滝川駅から帯広、釧路を経て根室駅を結ぶＪＲ北海道の路線です。このうち釧路駅から\
     根室駅までの区間は「花咲線」の愛称で呼ばれています。観光シーズンには札幌からのリゾート列車が多数運行されます。キハ283系の車体は、\
     ブルーとグリーンに丹頂鶴の赤を組み合わせ北海道らしさを演出しています."
@@ -11,53 +25,57 @@ sample_result='''
     {"千葉県-都道府県-千葉県": "https://ja.wikipedia.org/wiki/千葉県"},
 }
 '''
-system_message1="あなたは与えられた文章の中から都道府県名、市区町村名、駅名を表す単語を抜き出して\
-    関連したwikipediaの情報と併せてJSON形式で出力するエージェントです。なかった場合はnullを返してください."
-system_message2="あなたは与えられた文章の中からキーワードとタイトルを抜き出してJSON形式で出力するエージェントです"
-# チャットモデルで利用可能なメッセージの型をインポート
-from langchain.chat_models import ChatOpenAI
-# チャットモデルで利用可能なメッセージの型をインポート
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage,
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        MessagesPlaceholder(variable_name="history"),
+        SystemMessagePromptTemplate.from_template(
+            """あなたは以下の動画に関する説明文についての質問に回答するエージェントです。
+
+text:根室本線は北海道の滝川駅から帯広、釧路を経て根室駅を結ぶＪＲ北海道の路線です。
+このうち釧路駅から根室駅までの区間は「花咲線」の愛称で呼ばれています。
+観光シーズンには札幌からのリゾート列車が多数運行されます。
+キハ283系の車体は、ブルーとグリーンに丹頂鶴の赤を組み合わせ北海道らしさを演出しています.
+
+"""
+        ),
+        HumanMessagePromptTemplate.from_template("{input}"),
+    ]
 )
+prompt2=ChatPromptTemplate.from_messages(
+    [       
+        SystemMessagePromptTemplate.from_template(
+            """あなたは以下の動画に関する説明文についての質問に回答するエージェントです。
 
-# チャットモデルのラッパーを初期化
-chat = ChatOpenAI(temperature=0)
-# チャットモデルにメッセージを渡して、予測を受け取る
-# チャットモデルに渡すメッセージを作成する
+text:根室本線は北海道の滝川駅から帯広、釧路を経て根室駅を結ぶＪＲ北海道の路線です。
+このうち釧路駅から根室駅までの区間は「花咲線」の愛称で呼ばれています。
+観光シーズンには札幌からのリゾート列車が多数運行されます。
+キハ283系の車体は、ブルーとグリーンに丹頂鶴の赤を組み合わせ北海道らしさを演出しています.
 
-messages1 = [
-    SystemMessage(content=system_message1),
-    HumanMessage(content=sample_text),
-    AIMessage(content=sample_result),
-    HumanMessage(content=target_text),
-]
-result1 = chat(messages1)
-entity_list=result1.content
+"""
+        ),
+        MessagesPlaceholder(variable_name="history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+    ]
+
+)
+memory = ConversationBufferMemory(return_messages=True, memory_key="history")
+conversation = ConversationChain(memory=memory, prompt=prompt, llm=chat, verbose=True)
+memory.chat_memory.add_user_message(f"あなたは以下のテキスト{sample_text}から市町村名を抜き出して出力するエージェントです")
+memory.chat_memory.add_ai_message(f"{sample_result}")
+
+words_list = conversation.run(input="キーワードとタイトルをピックアップして")
+
+print(words_list)
+
+entity_list = conversation.run("例のように都道府県・市区町村・駅名をピックアップした後、関連したwikipdiaのリンクを表示してください")
+
 print(entity_list)
 
-messages2 =  [       
-    SystemMessage(content=system_message2),
-    HumanMessage(content=target_text),
-]
+target_JSON = conversation.run(
+    input=f"""説明文とピックアップしたキーワード {words_list}と都道府県・市区町村・駅名{entity_list}を使ってSchema.org の Clip クラスを用いて JSON-LD を生成して。
+ただし、キーワードは schema:keywords, タイトルは schema:name, 都道府県・市区町村は schema:location を使ってエンコードすること。
+"""
+)
 
-result2 =chat(messages2)
-keyward_list = result2.content
-print(keyward_list)
-
-from langchain.memory import ConversationBufferWindowMemory
-memory = ConversationBufferWindowMemory(k=4)
-memory.save_context({"input": f"{target_text}から都道府県名、市区町村名、駅名を表す単語を抜き出して\
-    関連したwikipediaの情報と併せてJSON形式で出力するエージェントです。なかった場合はnullを返してください."}, {"ouput": entity_list})
-memory.save_context({"input": f"{target_text}からキーワードとタイトルを抜き出してJSON形式で出力するエージェントです"}, {"ouput": keyward_list})
-log= memory.load_memory_variables({})
-
-messages=[
-    SystemMessage(content="動画の紹介文とそれから抜き出したデータを使いschema.orgのClipクラスを用いてJSON-LD形式で記述してください.\
-         typeがThingなものは除外してください"),
-    HumanMessage(content=f"{log}と{target_text}をもとにしてください")
-]
-result2 = chat(messages)
-print(result2)
+print(target_JSON)
