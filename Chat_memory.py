de740b1119e4768509db248106a0c873be223975
kotaro <big.i.land.k@gmail.com>
2023-06-20 16:48:21 +0900

    modified prompt

--- Chat_memory.py
+++ Chat_memory.py
@@ -9,6 +9,61 @@ from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 
+input_list=["東京駅","東京都","さいたま市"]
+example1="""
+{
+  "@context": "https://schema.org",
+  "@type": "Clip",
+  "name": "東京駅",
+  "location": {
+    "@type": "Place",
+    "name": "東京駅",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "千代田区",
+      "addressRegion": "東京都",
+      "postalCode": "100-0005",
+      "streetAddress": "東京都千代田区丸の内一丁目9-1"
+    "url": "https://ja.wikipedia.org/wiki/東京駅"
+    }
+  }
+}
+"""
+example2="""
+{
+  "@context": "https://schema.org",
+  "@type": "Clip",
+  "name": "東京都",
+  "location": {
+    "@type": "AdministrativeArea",
+    "name": "東京都",
+    "address": {
+      "@type": "PostalAddress",
+      "addressRegion": "東京都"
+    "url" : "https://ja.wikipedia.org/wiki/東京都"
+    }
+  }
+}
+"""
+
+example3="""
+{
+  "@context": "https://schema.org",
+  "@type": "Clip",
+  "name": "さいたま市",
+  "location": {
+    "@type": "City",
+    "name": "さいたま市",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "さいたま市",
+      "addressRegion": "埼玉県"
+    "url" : "https://ja.wikipedia.org/wiki/さいたま市"
+    }
+  }
+}
+"""
+output_list=[example1,example2,example3]
 
 chat = ChatOpenAI(temperature=0)
 
@@ -42,23 +97,6 @@ text:根室本線は北海道の滝川駅から帯広、釧路を経て根室駅
         HumanMessagePromptTemplate.from_template("{input}"),
     ]
 )
-prompt2=ChatPromptTemplate.from_messages(
-    [       
-        SystemMessagePromptTemplate.from_template(
-            """あなたは以下の動画に関する説明文についての質問に回答するエージェントです。
-
-text:根室本線は北海道の滝川駅から帯広、釧路を経て根室駅を結ぶＪＲ北海道の路線です。
-このうち釧路駅から根室駅までの区間は「花咲線」の愛称で呼ばれています。
-観光シーズンには札幌からのリゾート列車が多数運行されます。
-キハ283系の車体は、ブルーとグリーンに丹頂鶴の赤を組み合わせ北海道らしさを演出しています.
-
-"""
-        ),
-        MessagesPlaceholder(variable_name="history"),
-        HumanMessagePromptTemplate.from_template("{input}"),
-    ]
-
-)
 memory = ConversationBufferMemory(return_messages=True, memory_key="history")
 conversation = ConversationChain(memory=memory, prompt=prompt, llm=chat, verbose=True)
 memory.chat_memory.add_user_message(f"あなたは以下のテキスト{sample_text}から市町村名を抜き出して出力するエージェントです")
@@ -72,9 +110,14 @@ entity_list = conversation.run("例のように都道府県・市区町村・駅
 
 print(entity_list)
 
+for i in range(len(input_list)):
+    memory.chat_memory.add_user_message(f"あなたは与えられた単語{input_list[i]}についてSchema.orgを用いてjson-LDで記述するエージェントです。\
+                                        この時Clipクラスを用いて場所に関してはlocationプロパティを用いてください")
+    memory.chat_memory.add_ai_message(f"{output_list[i]}")
+
 target_JSON = conversation.run(
     input=f"""説明文とピックアップしたキーワード {words_list}と都道府県・市区町村・駅名{entity_list}を使ってSchema.org の Clip クラスを用いて JSON-LD を生成して。
-ただし、キーワードは schema:keywords, タイトルは schema:name, 都道府県・市区町村は schema:location を使ってエンコードすること。
+ただし、キーワードは schema:keywords, タイトルは schema:name, 都道府県・市区町村・駅名は schema:location を使ってエンコードすること。
 """
 )
 
--- experiment.py
+++ experiment.py
@@ -1,30 +1,53 @@
 from langchain import PromptTemplate
 from langchain import FewShotPromptTemplate
 from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import ConversationChain
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
-examples =[{'location': '東京', 'JSON': '以下は東京駅のJSON-LD記述例です。\n\n```json\n{\n  "@context": "https://schema.org",\n  "@type": "Place",\n  "name": "東京駅",\n  "address": {\n    "@type": "PostalAddress",\n    "addressLocality": "千代田区",\n    "addressRegion": "東京都",\n    "postalCode": "100-0005",\n    "streetAddress": "丸の内一丁目9-1"\n  },\n  "geo": {\n    "@type": "GeoCoordinates",\n    "latitude": 35.681236,\n    "longitude": 139.767125\n  },\n  "openingHoursSpecification": [\n    {\n      "@type": "OpeningHoursSpecification",\n      "dayOfWeek": [\n        "Monday",\n        "Tuesday",\n        "Wednesday",\n        "Thursday",\n        "Friday"\n      ],\n      "opens": "05:00",\n      "closes": "01:00"\n    },\n    {\n      "@type": "OpeningHoursSpecification",\n      "dayOfWeek": [\n        "Saturday",\n        "Sunday"\n      ],\n      "opens": "05:00",\n      "closes": "24:00"\n    }\n  ],\n  "telephone": "+81-3-3210-0200",\n  "url": "https://www.tokyostationcity.com/"\n}\n```\n\nこの例では、`@type`に`Place`を指定し、`name`には"東京駅"を指定しています。また、`address`には`PostalAddress`を指定し、住所情報を記述しています。`geo`には緯度経度情報を指定し、`openingHoursSpecification`には営業時間を指定しています。`telephone`には電話番号を、`url`には公式ウェブサイトのURLを指定しています。'}, {'location': '東京', 'JSON': '以下は「さいたま市」に関するJSON-LDの例です。\n\n```json\n{\n  "@context": "https://schema.org/",\n  "@type": "City",\n  "name": "さいたま市",\n  "location": {\n    "@type": "Place",\n    "address": {\n      "@type": "PostalAddress",\n      "addressLocality": "さいたま市"\n    }\n  }\n}\n```\n\nこの例では、`@type`プロパティに`City`を指定し、`name`プロパティに「さいたま市」を指定しています。また、`location`プロパティには、`Place`を指定し、`address`プロパティに`PostalAddress`を指定しています。`addressLocality`プロパティにも「さいたま市」を指定しています。これにより、`location`プロパティが`PostalAddress`であることと、`addressLocality`プロパティが「さいたま市」であることが示されます。'}, {'location': '東京', 'JSON': '以下は神奈川駅のJSON-LD記述例です。\n\n```json\n{\n  "@context": "https://schema.org",\n  "@type": "Place",\n  "name": "神奈川駅",\n  "address": {\n    "@type": "PostalAddress",\n    "addressLocality": "神奈川市",\n    "addressRegion": "神奈川県",\n    "postalCode": "221-0056",\n    "streetAddress": "神奈川区鶴屋町1-1"\n  },\n  "geo": {\n    "@type": "GeoCoordinates",\n    "latitude": 35.4714,\n    "longitude": 139.6242\n  },\n  "openingHoursSpecification": [\n    {\n      "@type": "OpeningHoursSpecification",\n      "dayOfWeek": [\n        "Monday",\n        "Tuesday",\n        "Wednesday",\n        "Thursday",\n        "Friday"\n      ],\n      "opens": "05:00",\n      "closes": "01:00"\n    },\n    {\n      "@type": "OpeningHoursSpecification",\n      "dayOfWeek": [\n        "Saturday",\n        "Sunday"\n      ],\n      "opens": "05:00",\n      "closes": "24:00"\n    }\n  ],\n  "telephone": "+81-45-441-1111",\n  "url": "https://www.jreast.co.jp/estation/station/info.aspx?StationCd=111"\n}\n```\n\nこの例では、神奈川駅を表す `Place` タイプのオブジェクトを定義しています。`name` プロパティには駅名を、`address` プロパティには住所を、`geo` プロパティには緯度経度を、`openingHoursSpecification` プロパティには営業時間を、`telephone` プロパティには電話番号を、`url` プロパティには公式ウェブサイトのURLをそれぞれ指定しています。また、住所は `PostalAddress` タイプのオブジェクトで表現され、`addressLocality` プロパティには市区町村名、`addressRegion` プロパティには都道府県名、`postalCode` プロパティには郵便番号、`streetAddress` プロパティには番地以下の住所をそれぞれ指定しています。営業時間は `OpeningHoursSpecification` タイプのオブジェクトで表現され、`dayOfWeek` プロパティには曜日、`opens` プロパティには開店時間、`closes` プロパティには閉店時間をそれぞれ指定しています。'}]
 
-example_formatter_template = """
-フルーツ: {location}
-色: {JSON}\n
+chat = ChatOpenAI(temperature=0)
+
+target_text= "根室本線は北海道の滝川駅から帯広、釧路を経て根室駅を結ぶＪＲ北海道の路線です。このうち釧路駅から\
+    根室駅までの区間は「花咲線」の愛称で呼ばれています。観光シーズンには札幌からのリゾート列車が多数運行されます。キハ283系の車体は、\
+    ブルーとグリーンに丹頂鶴の赤を組み合わせ北海道らしさを演出しています."
+sample_text = "三郷市は、埼玉県南東部にあり、東京都と千葉県に接しています。名物の小松菜はハウス栽培で１年を通して出荷されます。\
+         小松菜を使った新しい料理にも取り組んでいます。「小松菜餃子」は、小松菜パウダーを皮に練りこみ、具にも小松菜がたっぷりと入っています。"
+sample_result='''
+{
+    {"三郷市-市-三郷市": "https://ja.wikipedia.org/wiki/三郷市"},
+    {"埼玉県-都道府県-埼玉県": "https://ja.wikipedia.org/wiki/埼玉県"},
+    {"東京都-都道府県-東京都": "https://ja.wikipedia.org/wiki/東京都"},
+    {"千葉県-都道府県-千葉県": "https://ja.wikipedia.org/wiki/千葉県"},
+}
+'''
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+
+        SystemMessagePromptTemplate.from_template(
+            """あなたは与えられた単語についてSchema.orgを用いてjson-LDで記述するエージェントです。
+            この時Clipクラスを用いて場所に関してはlocationプロパティを用いてください
 """
-example_prompt = PromptTemplate(
-    template=example_formatter_template,
-    input_variables=["location", "JSON"]
+        ),
+        MessagesPlaceholder(variable_name="history"),
+        HumanMessagePromptTemplate.from_template("{input}"),
+    ]
 )
+memory = ConversationBufferMemory(return_messages=True, memory_key="history")
+conversation = ConversationChain(memory=memory, prompt=prompt, llm=chat, verbose=True)
 
-few_shot_prompt = FewShotPromptTemplate(
-    examples=examples,
-    example_prompt=example_prompt,
-    prefix="あなたは与えられた単語に関してShcema.orgのJSON-LDを用いて記述するエージェントです。この時schema:location を使ってエンコードしてください",
-    suffix="フルーツ: {input}\nJSON:",
-    input_variables=["input"],
-    example_separator="\n\n",
+words_list = ["東京駅","東京都","さいたま市"]
 
-)
+for word in words_list:
+    result = conversation.run(input=word)
+    print(result)
 
-prompt_text = few_shot_prompt.format(input="武蔵浦和駅")
-print(prompt_text)
 
-llm = OpenAI(model_name="text-davinci-003")
-print(llm(prompt_text))
